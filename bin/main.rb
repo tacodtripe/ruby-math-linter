abe2bbcf236348d0a2737ed9b4c03dbfc2aaf812
tacodtripe <55043949+tacodtripe@users.noreply.github.com>
2020-07-14 09:30:57 -0600

    add spacing method

---
+++ bin/main.rb
@@ -0,0 +1,10 @@
+require_relative '../lib/reader.rb'
+
+# require_relative '../lib/symbols.rb'
+
+file = '../test.txt'
+line_number = 1
+File.readlines(file).each do |line|
+  Spacing.spacing(line, line_number)
+  line_number += 1
+end
---
+++ lib/reader.rb
@@ -0,0 +1,17 @@
+require 'strscan'
+require 'colorize'
+
+class Spacing
+  def self.spacing(string, line)
+    arr = string.chars
+    arr.each_with_index do |n, i|
+      next unless (n == '+' || n == '-') && (arr[i - 1] != ' ' || arr[i + 1] != ' ') # rubocop:disable Style/MultipleComparison
+
+      print '- Make sure to apply correct space between'.colorize(:white)
+      print " #{n}".colorize(:magenta)
+      print ' at line'.colorize(:white)
+      print " #{line}".colorize(:green)
+      puts ''
+    end
+  end
+end
